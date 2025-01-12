a5b98ef921ddb7222c03437fc8ae3e59a9fce283
Luboš Komjati <dev@casp.cz>
2025-01-12 10:13:48 +0100

    customized to match project name

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,11 +1,14 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Články' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/categories', title: 'Rubriky' },
-  { href: '/rubriky/sex', title: 'Sex' },
+  // { href: '/tags', title: 'Tags' },
+  // { href: '/categories', title: 'Rubriky' },
+  { href: '/tags/sex', title: 'Sex' },
+  { href: '/tags/zivot', title: 'Život' },
+  { href: '/tags/bydleni', title: 'Bydlení' },
+  { href: '/tags/zdravi', title: 'Zdraví' },
   // { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/about', title: 'O nás' },
 ]
 
 export default headerNavLinks
